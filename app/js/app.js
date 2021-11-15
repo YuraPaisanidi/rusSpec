1864d9c5a8281b486eb9a8243b4a71f1d3ab494d
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-11-15 17:44:58 +0200

    about section

--- app/js/app.js
+++ app/js/app.js
@@ -298,4 +298,28 @@ document.addEventListener("DOMContentLoaded", function() {
 		forms('.form');
 
 });
-	
+
+
+$(document).ready(function () {
+ 
+	var show = true;
+	var countbox = ".about__numbers";
+	$(window).on("scroll load resize", function () {
+			if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+			var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+			var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+			var w_height = $(window).height(); // Высота окна браузера
+			var d_height = $(document).height(); // Высота всего документа
+			var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+			if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+					$('.about__numbers_wrap').css('opacity', '1');
+					$('.about__numbers_count').spincrement({
+							thousandSeparator: "",
+							duration: 3000
+					});
+					 
+					show = false;
+			}
+	});
+
+});
