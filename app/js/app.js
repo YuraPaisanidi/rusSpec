9c11d755bfcb7d71cfeb220b66bf5efd462e9836
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-11-17 16:20:17 +0200

    index ready

--- app/js/app.js
+++ app/js/app.js
@@ -299,7 +299,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-
+//------------------------------------Number count-----------------------------
 $(document).ready(function () {
  
 	var show = true;
@@ -324,6 +324,7 @@ $(document).ready(function () {
 
 });
 
+//---------------------------TABS----------------------------
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
 	const header = document.querySelector(headerSelector),
 				tab = document.querySelectorAll(tabSelector),
@@ -362,3 +363,20 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
 	});
 };
 tabs('.rentTabs', '.rentItem', '.rentWrap', 'active');
+
+//---------------------------WOW js----------------------------
+var wow = new WOW(
+	{
+		boxClass:     'wow',      // animated element css class (default is wow)
+		animateClass: 'animated', // animation css class (default is animated)
+		offset:       0,          // distance to the element when triggering the animation (default is 0)
+		mobile:       true,       // trigger animations on mobile devices (default is true)
+		live:         true,       // act on asynchronously loaded content (default is true)
+		callback:     function(box) {
+			// the callback is fired every time an animation is started
+			// the argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null // optional scroll container selector, otherwise use window
+	}
+);
+wow.init();
